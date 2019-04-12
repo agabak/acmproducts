201fec9432baff98612a5844d08e0a3e13a6a5d4
Agaba Kabunga <agaba_k@hotmail.com>
2019-04-12 14:54:17 -0500

    Add  Customer class and its proparties

---
+++ acmproduct/ACM.BL/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        private string _lastName;
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public  string MyProperty
+        {
+            get { return LastName + "," +  FirstName; }
+        }
+
+    }
+
+}
